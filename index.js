34ffbec1789244c47611464c80a4a13dc783ff9b
reddevilcero <reddevil_cero@hotmail.com>
2020-04-17 16:03:52 +0100

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 function fetchBooks() {
-
-}
-
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
+  return fetch("https://anapioficeandfire.com/api/books/",{mode: 'cors'}).then((response) => {
+    return response.json()
+  }).then((json) => {
+    renderBooks(json)
   })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+function renderBooks(json) {
+  const main = document.querySelector("main");
+  json.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = `<h2>${book.name}</h2>`;
+    main.appendChild(h2);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks();
+});
